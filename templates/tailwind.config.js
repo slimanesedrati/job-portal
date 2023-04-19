ea23852ddc3cfdcd37764e62cba0a7006c78e84e
Slimane <slimane.sedrati@univ-bba.dz>
2023-04-19 15:23:07 +0100

    update

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        cyan: 'hsl(180, 66%, 49%)',
-        cyanLight: 'hsl(180, 66%, 69%)',
-        darkViolet: 'hsl(257, 27%, 26%)',
         darkGray:'hsl(210, 4%, 20%)',
         mediumGray:'hsl(210, 2%, 53%)',
         lightGray:'hsl(210, 4%, 80%)',
