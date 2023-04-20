88907d26b6409fe22d776de61e038f6a714ceeb0
daishek <daichekkal@gmail.com>
2023-04-20 19:16:32 +0100

    navbar,, footer + basic components

---
+++ client/app/components/Navbar/NavbarList.tsx
@@ -0,0 +1,12 @@
+'use client'
+const NavbarList = () => {
+  return (
+    <div className="hidden text-sm  space-x-8 font-bold lg:flex">
+        <a href="#" className="text-gray-med hover:text-black">Jobs</a>
+        <a href="#" className="text-gray-med hover:text-black">Interships</a>
+        <a href="#" className="text-gray-med hover:text-black">About Us</a>
+    </div>
+  )
+}
+
+export default NavbarList
---
+++ client/app/components/Navbar/index.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { useState } from "react"
+import { Button, Logo }from ".."
+import NavbarList from "./NavbarList"
+import NavbarMenuButton from "./NavbarMenuButton"
+import NavbarMobileMenu from "./NavbarMobileMenu"
+import Link from "next/link"
+
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="sticky top-0 bg-white shadow-sm z-40">
+      <nav className="relative container mx-auto p-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-20">
+            <Link href='/'>
+              <Logo />
+            </Link>
+            <NavbarList />
+          </div>
+
+          <div className="hidden items-center text-sm font-bold text-mediumGray lg:flex">
+            <Button asLink url="login" className="bg-transparent text-gray-med">Login</Button>
+            <Button asLink url="signup" className="text-white">Signup</Button>
+          </div>
+
+          <NavbarMenuButton isOpen setIsOpen={setIsOpen} />
+          
+        </div>
+        {isOpen&&(
+          <NavbarMobileMenu />
+        )}
+
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
